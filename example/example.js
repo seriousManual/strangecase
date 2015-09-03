eea0cbc0aac50f1cf9b98f0317d88cc755ebea7e
zaphod1984 <zaphod84@gmx.de>
2015-09-03 09:32:34 +0200

    initial

---
+++ example/example.js
@@ -0,0 +1,8 @@
+var strangeCase = require('../index');
+
+var testText = 'this is a very long test that should test.'
+
+console.log(strangeCase(testText));
+console.log(strangeCase(testText, 0.1));
+console.log(strangeCase(testText, 0.6));
+console.log(strangeCase(testText, 0.8));
